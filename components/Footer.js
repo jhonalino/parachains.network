7a32a8d7a47bd2a3a4d1107248c30f2a2907b17e
jhonalino <jhonfrancisalino@gmail.com>
2021-06-10 07:05:14 -0700

    added tip address

--- components/Footer.js
+++ components/Footer.js
@@ -2,8 +2,8 @@ import Link from 'next/link';
 
 export default function Footer() {
     return (
-        <div className="h-16 w-full border border-t border-gray-700 border-opacity-10 mt-2 bg-white">
-            <div className="max-w-screen-xl m-auto flex justify-between items-center h-full p-2">
+        <div className="w-full border border-t border-gray-700 border-opacity-10 mt-2 bg-white">
+            <div className="max-w-screen-xl m-auto flex flex-wrap justify-between items-center h-full p-2 overflow-x-auto">
                 <p>
                     <Link href="/">
                         <a>
@@ -14,7 +14,14 @@ export default function Footer() {
                         <a className="ml-1">polkaview</a>
                     </Link>
                 </p>
-
+                <div className="flex flex-col text-gr">
+                    <div>
+                        <span className="mr-2" style={{ color: '#e6007a' }}>TIP (DOT):</span><span>13Tbq9Exz6r9GzEYxiey6QEQPrnhvecxNv2SPiSMny6CFWqY</span>
+                    </div>
+                    <div>
+                        <span className="mr-2" style={{ color: '#FF8F00' }}>TIP (KSM):</span><span>F2vM8Kmkgbbb73UmnR1rCmFgq5J31szko8hd5ixigHApDNY</span>
+                    </div>
+                </div>
             </div>
         </div>
     );
